d8c590b80c783f358f1e99ce94b5322d47918bf1
Aaravee456 <101640088+Aaravee456@users.noreply.github.com>
2023-03-23 15:33:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function phone_screen() {
+window.location = "phone.html";
+}
+
+function tablet_screen() {
+window.location = "tablet.html";
+}
+
+function car_screen() {
+window.location = "car.html";
+}
+
+function house_screen() {
+window.location = "house.html";
+}
